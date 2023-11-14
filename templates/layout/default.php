12517ad8cfba7e77138d69b892e455468665bccd
Krzanowski-Ambroise <97064876+Krzanowski-Ambroise@users.noreply.github.com>
2023-11-14 11:28:13 +0100

    layout

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -38,10 +38,14 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
         <div class="top-nav-title">
             <a href="<?= $this->Url->build('/') ?>"><span>Gsb</span>PHP</a>
         </div>
-        <div class="top-nav-links">
-            <a target="_blank" rel="noopener" href="https://book.cakephp.org/4/">Documentation</a>
-            <a target="_blank" rel="noopener" href="https://api.cakephp.org/">API</a>
-        </div>
+        <?php
+            echo $this->Html->Link('Home', ['plugin' => NULL, 'controller' => 'Pages', 'action' => 'home']);
+            echo $this->Html->Link('Users', ['plugin' => 'CakeDC/Users','controller' => 'Users', 'action' => 'index']);
+            echo $this->Html->Link('My sheets', ['plugin' => NULL, 'controller' => 'Sheets', 'action' => 'index']);
+            echo $this->Html->Link('Packages', ['plugin' => NULL, 'controller' => 'packages', 'action' => 'index']);
+            echo $this->Html->Link('Out packages', ['plugin' => NULL, 'controller' => 'outpackages', 'action' => 'index']);
+            echo $this->Html->Link('Logout' , ['plugin' => 'CakeDC/Users', 'controller' => 'Users', 'action' => 'logout'], ['onclick' => "return confirm('Etes-vous sûr de vouloir vous déconnecter ?')"]);
+        ?>
     </nav>
     <main class="main">
         <div class="container">
